ca1eab0fd57e965d8615d29b84af050565cd83bc
zakashour <zak.ashour@trucesoftware.com>
2024-06-15 06:31:57 -0500

    Initial Commit

---
+++ iamrich_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iamrich_swiftui
+//
+//  Created by Zak Ashour on 6/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iamrich_swiftui/iamrich_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iamrich_swiftuiApp.swift
+//  iamrich_swiftui
+//
+//  Created by Zak Ashour on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct iamrich_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
